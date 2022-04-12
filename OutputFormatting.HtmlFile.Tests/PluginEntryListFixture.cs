6e39ffc7dd1191460a8c5e62fc19a1b1b8c2e5c9
Paulo Mouat <paulo.mouat@gmail.com>
2022-04-12 17:12:59 -0400

    Added a couple of tests covering entry lists.

---
+++ OutputFormatting.HtmlFile.Tests/PluginEntryListFixture.cs
@@ -0,0 +1,95 @@
+using System.Linq;
+using System.Xml.Linq;
+using FluentAssertions;
+using ListVst;
+using ListVst.OutputFormatting.HtmlFile;
+using Moq;
+using Xunit;
+
+namespace OutputFormatting.HtmlFile.Tests;
+
+public class PluginEntryListFixture
+{
+    [Fact]
+    public void AddDescriptors_RendersAll()
+    {
+        var descriptors = new[]
+        {
+            new ProjectDescriptor("/root/sub1/file1.ext"),
+            new ProjectDescriptor("/root/sub1/file2.ext")
+        };
+
+        var sut = GetSubject();
+        var outputElement = new XElement("output");
+        sut.Add(descriptors, outputElement);
+
+        outputElement.ToString().Should().Be(
+@"<output>
+  <div class=""item"">
+    <a href=""#root-sub1-file1-ext"">/root/sub1/file1.ext</a>
+  </div>
+  <div class=""item"">
+    <a href=""#root-sub1-file2-ext"">/root/sub1/file2.ext</a>
+  </div>
+</output>"
+        );
+    }
+
+    [Fact]
+    public void AddLookup_RendersAll()
+    {
+        var projects = new[]
+        {
+            new ProjectDescriptor("/root/sub1/file1.ext"),
+            new ProjectDescriptor("/root/sub1/file2.ext")
+        };
+
+        var plugins = new[]
+        {
+            new PluginDescriptor("plugin11", "manufacturer1", "manufacturer1 plugin11"),
+            new PluginDescriptor("plugin12", "manufacturer1", "manufacturer1 plugin12"),
+        };
+
+        var mapping = new[]
+        {
+            new { PluginDescriptor = plugins[0], ProjectDescriptor = projects[0] },
+            new { PluginDescriptor = plugins[1], ProjectDescriptor = projects[0] },
+            new { PluginDescriptor = plugins[0], ProjectDescriptor = projects[1] },
+        };
+        
+        var lookup = mapping.ToLookup(p => p.PluginDescriptor, p => p.ProjectDescriptor);
+
+        var sut = GetSubject();
+        sut.Add(lookup);
+
+        sut.ToString().Should().Be(
+@"<div id=""mockId"">
+  <div id=""manufacturer1-plugin11"" class=""entry"">
+    <div class=""key title"">manufacturer1 plugin11</div>
+    <a class=""link-to-top"" href=""#document-title"">top</a>
+    <a class=""link-to-section"" href=""#"">section index</a>
+    <div class=""item"">
+      <a href=""#root-sub1-file1-ext"">/root/sub1/file1.ext</a>
+    </div>
+    <div class=""item"">
+      <a href=""#root-sub1-file2-ext"">/root/sub1/file2.ext</a>
+    </div>
+  </div>
+  <div id=""manufacturer1-plugin12"" class=""entry"">
+    <div class=""key title"">manufacturer1 plugin12</div>
+    <a class=""link-to-top"" href=""#document-title"">top</a>
+    <a class=""link-to-section"" href=""#"">section index</a>
+    <div class=""item"">
+      <a href=""#root-sub1-file1-ext"">/root/sub1/file1.ext</a>
+    </div>
+  </div>
+</div>");
+    }
+
+    private PluginEntryList GetSubject()
+    {
+        var sectionMock = Mock.Of<ISection>();
+        var sut = new PluginEntryList("mockId", sectionMock);
+        return sut;
+    }
+}
---
+++ OutputFormatting.HtmlFile.Tests/ProjectEntryListFixture.cs
@@ -0,0 +1,95 @@
+using System.Linq;
+using System.Xml.Linq;
+using FluentAssertions;
+using ListVst;
+using ListVst.OutputFormatting.HtmlFile;
+using Moq;
+using Xunit;
+
+namespace OutputFormatting.HtmlFile.Tests;
+
+public class ProjectEntryListFixture
+{
+    [Fact]
+    public void RendersAllDescriptors()
+    {
+        var descriptors = new[]
+        {
+            new PluginDescriptor("plugin11", "manufacturer1", "manufacturer1 plugin11"),
+            new PluginDescriptor("plugin21", "manufacturer2", "manufacturer2 plugin21"),
+        };
+
+        var sut = GetSubject();
+        var outputElement = new XElement("output");
+        sut.Add(descriptors, outputElement);
+
+        outputElement.ToString().Should().Be(
+@"<output>
+  <div class=""item"">
+    <a href=""#manufacturer1-plugin11"">manufacturer1 plugin11</a>
+  </div>
+  <div class=""item"">
+    <a href=""#manufacturer2-plugin21"">manufacturer2 plugin21</a>
+  </div>
+</output>"
+        );
+    }
+
+    [Fact]
+    public void AddLookup_RendersAll()
+    {
+        var projects = new[]
+        {
+            new ProjectDescriptor("/root/sub1/file1.ext"),
+            new ProjectDescriptor("/root/sub1/file2.ext")
+        };
+
+        var plugins = new[]
+        {
+            new PluginDescriptor("plugin11", "manufacturer1", "manufacturer1 plugin11"),
+            new PluginDescriptor("plugin12", "manufacturer1", "manufacturer1 plugin12"),
+        };
+
+        var mapping = new[]
+        {
+            new { ProjectDescriptor = projects[0], PluginDescriptor = plugins[0] },
+            new { ProjectDescriptor = projects[0], PluginDescriptor = plugins[1] },
+            new { ProjectDescriptor = projects[1], PluginDescriptor = plugins[0] },
+        };
+        
+        var lookup = mapping.ToLookup(p => p.ProjectDescriptor, p => p.PluginDescriptor);
+
+        var sut = GetSubject();
+        sut.Add(lookup);
+
+        sut.ToString().Should().Be(
+            @"<div id=""mockId"">
+  <div id=""root-sub1-file1-ext"" class=""entry"">
+    <div class=""key title"">/root/sub1/file1.ext</div>
+    <a class=""link-to-top"" href=""#document-title"">top</a>
+    <a class=""link-to-section"" href=""#"">section index</a>
+    <div class=""item"">
+      <a href=""#manufacturer1-plugin11"">manufacturer1 plugin11</a>
+    </div>
+    <div class=""item"">
+      <a href=""#manufacturer1-plugin12"">manufacturer1 plugin12</a>
+    </div>
+  </div>
+  <div id=""root-sub1-file2-ext"" class=""entry"">
+    <div class=""key title"">/root/sub1/file2.ext</div>
+    <a class=""link-to-top"" href=""#document-title"">top</a>
+    <a class=""link-to-section"" href=""#"">section index</a>
+    <div class=""item"">
+      <a href=""#manufacturer1-plugin11"">manufacturer1 plugin11</a>
+    </div>
+  </div>
+</div>");
+    }
+    
+    private ProjectEntryList GetSubject()
+    {
+        var sectionMock = Mock.Of<ISection>();
+        var sut = new ProjectEntryList("mockId", sectionMock);
+        return sut;
+    }
+}
