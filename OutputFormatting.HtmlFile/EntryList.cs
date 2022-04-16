21bbd3b0352d2237913ebc58dd503d5b6b07b018
Paulo Mouat <paulo.mouat@gmail.com>
2022-04-16 18:28:11 -0400

    Preliminary refactor to allow the entry title to have better formatting.

--- OutputFormatting.HtmlFile/EntryList.cs
+++ OutputFormatting.HtmlFile/EntryList.cs
@@ -21,34 +21,44 @@ public class EntryList<TEntry, TItem> : XElement, IEntryList
     {
         foreach(var group in lookup)
         {
-            var key = GetKey(group.Key);
-            var entryId = new Id(key).Value;
-            
-            var entry = new XElement("div",
-                new XAttribute("id", entryId),
-                new XAttribute("class", "entry"));
-            
-            var entryTitle = new XElement("div",
-                new XAttribute("class", "key title"),
-                key);
-            entry.Add(entryTitle);
-            var linkToTop = new XElement("a",
-                new XAttribute("class", "link-to-top"),
-                new XAttribute("href", "#document-title"),
-                "top");
-            entry.Add(linkToTop);
-            var linkToSection = new XElement("a",
-                new XAttribute("class", "link-to-section"),
-                new XAttribute("href", "#" + ParentSection.Id),
-                "section index");
-            entry.Add(linkToSection);
-            
+            var entry = BuildEntry(group);
+            AddHeadings(entry);
             AddItemsToEntry(group, entry);
             Add(entry);
         }
     }
 
-    public virtual void AddItemsToEntry(IEnumerable<TItem> item, XElement entry)
+    public virtual XElement BuildEntry(IGrouping<TEntry, TItem> group)
+    {
+        var key = GetKey(group.Key);
+        var entryId = new Id(key).Value;
+            
+        var entry = new XElement("div",
+            new XAttribute("id", entryId),
+            new XAttribute("class", "entry"));
+            
+        var entryTitle = new XElement("div",
+            new XAttribute("class", "key title"),
+            key);
+        entry.Add(entryTitle);
+        return entry;
+    }
+    
+    public virtual void AddHeadings(XElement entry)
+    {
+        var linkToTop = new XElement("a",
+            new XAttribute("class", "link-to-top"),
+            new XAttribute("href", "#document-title"),
+            "top");
+        entry.Add(linkToTop);
+        var linkToSection = new XElement("a",
+            new XAttribute("class", "link-to-section"),
+            new XAttribute("href", "#" + ParentSection.Id),
+            "section index");
+        entry.Add(linkToSection);
+    }
+    
+    public virtual void AddItemsToEntry(IEnumerable<TItem> items, XElement entry)
     { }
 
     protected virtual string GetKey(TEntry entry)
